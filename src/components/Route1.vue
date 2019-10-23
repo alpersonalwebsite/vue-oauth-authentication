e258d104955ddfccfbeeccad538288e89cb0acc1
Al Diaz <mypersonalwebsite@outlook.com>
2019-10-23 09:10:15 -0700

    google's oauth, components

---
+++ src/components/Route1.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      Route 1
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Route1'
+}
+</script>
---
+++ src/components/Route2.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      Route 2
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Route2'
+}
+</script>
